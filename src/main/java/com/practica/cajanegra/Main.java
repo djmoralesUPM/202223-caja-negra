941e41a596277922f9589811e642daa8c0137b14
djmoralesUPM <d.jmorales@alumnos.upm.es>
2022-11-27 17:29:09 +0100

    added test class

--- src/main/java/com/practica/cajanegra/Main.java
+++ src/main/java/com/practica/cajanegra/Main.java
@@ -3,6 +3,6 @@ package com.practica.cajanegra;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Starting tests...\n\n\n");
     }
 }
