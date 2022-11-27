e4bedc77a22d3a91d94fbf3e0b6a7218f14c1058
djmoralesUPM <d.jmorales@alumnos.upm.es>
2022-11-27 17:30:20 +0100

    added test class

---
+++ src/test/java/com/practica/cajanegra/TestsBinaryTree.java
@@ -0,0 +1,24 @@
+package com.practica.cajanegra;
+
+/*
+Métodos a probar:
+getRoot(): Node<T>
+size(): int
+insert(content: T, atNode: Node<T>, atLeft: boolean): void
+search(targetContent: T): Node<T>
+remove(node: Node<T>): void
+equals(binaryTree: BinaryTree): boolean
+toList(): ArrayList<T>
+iterator(): Iterator<T>
+getSubTree(node: Node<T>): BinaryTree<T>
+depth(): int
+depth(node: Node<T>): int
+toString(): String
+(los siguientes no sé porque son privados)
+recursiveSearch(content: T, atNode: Node<T>)
+recursiveInsert(newNode: Node<T>, atNode: Node<T>, boolean: atLeft)
+ */
+
+public class TestsBinaryTree {
+
+}
